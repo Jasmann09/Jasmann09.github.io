6f11656be8aaef0a932b7effb3619153e363bc20
Jasmann09 <js0289@srmist.edu.in>
2022-09-04 12:03:47 +0530

    last

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,8 +3,8 @@ const authController = require('../controllers/authController');
 
 const router = Router();
 
-router.get('/signup', authController.signup_get);
-router.post('/signup', authController.signup_post);
+router.get('/', authController.signup_get);
+router.post('/singup', authController.signup_post);
 router.get('/login', authController.login_get);
 router.post('/login', authController.login_post);
 router.get('/logout', authController.logout_get);
